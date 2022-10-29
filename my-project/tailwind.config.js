c4924b842cbdf62f3cea5881d3c72384a0181a9a
shelynna <sheilamakynhills@gmail.com>
2022-10-29 17:30:03 +0000

    installed packages for the projects

---
+++ my-project/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+// const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+
+    // plugin(function ({ addComponents }) {
+    //   addComponents({
+    //     '.btn': {
+    //       padding: '.5rem 1rem',
+    //       borderRadius: '.25rem',
+    //       fontWeight: '600',
+    //     },
+    // '.btn-blue': {
+    //   backgroundColor: '#3490dc',
+    //   color: '#fff',
+    //   '&:hover': {
+    //     backgroundColor: '#2779bd'
+    //   },
+    // },
+    // '.btn-red': {
+    //   backgroundColor: '#e3342f',
+    //   color: '#fff',
+    //   // '&:hover': {
+    //   //   backgroundColor: '#cc1f1a'
+    //   // },
+    // },
+    //   })
+    // })
+  ]
+}
